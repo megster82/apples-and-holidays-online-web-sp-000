8b3d31245eaec27ff9a34cc4993707c473aab774
meaghan jennings <meaghanljennings@gmail.com>
2021-03-11 01:15:37 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -1,21 +1,4 @@
-def second_supply_for_fourth_of_july(holiday_hash)
-   {
-     :winter => {
-       :christmas => ["Lights", "Wreath"],
-       :new_years => ["Party Hats"]
-     },
-     :summer => {
-       :fourth_of_july => ["Fireworks", "BBQ"]
-     },
-     :fall => {
-       :thanksgiving => ["Turkey"]
-     },
-     :spring => {
-       :memorial_day => ["BBQ"]
-     }
-   }
-  holiday_hash[:summer][:fourth_of_july][1]
-end
+de
 
 def add_supply_to_winter_holidays(holiday_hash, supply)
   holiday_hash[:winter].each do |holiday, decorations|
